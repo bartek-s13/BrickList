e2c97f1cc786f06929392e0b873b53f567dd1aba
bartek-s13 <43931596+bartek-s13@users.noreply.github.com>
2020-05-16 10:51:55 +0200

    add layout

--- app/src/main/java/com/example/bricklist/MainActivity.kt
+++ app/src/main/java/com/example/bricklist/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.bricklist
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.widget.Toolbar
 import androidx.preference.PreferenceManager
 
@@ -25,6 +27,11 @@ class MainActivity : BaseActivity() {
     fun readConfig(){
         val preferences = PreferenceManager.getDefaultSharedPreferences(this)
         this.show_archived = preferences.getBoolean("show_archived ", false)
+    }
+
+    fun addNewInventory(v: View){
+        val intent = Intent(this, AddInventoryActivity::class.java)
+        startActivity(intent)
     }
 
 }
